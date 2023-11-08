1ff13a9c1e0a036456bf89971d37aea213084467
Jordan DOULIEZ <jordandouliez@gmail.com>
2023-11-08 11:27:18 +0100

    Refactor

--- ffuf-launcher.py
+++ ffuf-launcher.py
@@ -43,8 +43,12 @@ if find_executable("fzf") is None:
     sys.exit(1)
 
 if find_executable("ffuf") is None:
-    print(f"{Fore.RED}[!] ffuf tool is not installed (https://github.com/ffuf/ffuf).\n Please run \"go install github.com/ffuf/ffuf@latest\"{Style.RESET_ALL}")
+    print(f"{Fore.RED}[!] ffuf tool is not installed (https://github.com/ffuf/ffuf).\n Please run \"go install github.com/ffuf/ffuf/v2@latest\"{Style.RESET_ALL}")
     sys.exit(1)
+
+scans_path = "./scans"
+if not os.path.isdir(scans_path):
+    os.makedirs(scans_path)
 
 #########################
 # Configuration
@@ -56,7 +60,7 @@ WORDLISTS_CHOICES = {
     "directory-list-2.3-medium.txt": "/usr/share/SecLists/Discovery/Web-Content/directory-list-2.3-medium.txt",
     "directory-list-2.3-big.txt": "/usr/share/SecLists/Discovery/Web-Content/directory-list-2.3-big.txt",
     "Bo0oM-fuzz.txt": "/usr/share/Bo0oM-fuzz.txt",
-    "onelistforallmicro.txt": "/opt/OneListForAll/onelistforallmicro.txt",
+    "onelistforallmicro.txt": "/usr/share/onelistforallmicro.txt",
     "jhaddix_content_discovery_all.txt": "/usr/share/jhaddix_content_discovery_all.txt"
 }
 
@@ -71,23 +75,23 @@ CUSTOM_LIST = "Custom list"
 #########################
 wordlists_question = [inquirer.List('choice',message="Which wordlist do you want to fuzz with?", choices=[*WORDLISTS_CHOICES, CUSTOM_LIST])]
 wordlist_answer = inquirer.prompt(wordlists_question)["choice"]
-
+2
 if wordlist_answer == CUSTOM_LIST:
     output = subprocess.check_output("find " + " ".join(WORDLISTS_FOLDER_PATHS) + " -type f -name '*.txt' 2>/dev/null | fzf", shell=True)
     wordlist = output.decode('utf-8').splitlines()[0]
-else: 
+else:
     wordlist = WORDLISTS_CHOICES[wordlist_answer]
 
 if not os.path.exists(wordlist):
     print(f"{Fore.RED}[!] The selected wordlist is not accessible. Check the configuration section in the file.{Style.RESET_ALL}")
-    sys.exit(1)    
+    sys.exit(1)
 
 #########################
 # Selecting extensions
 #########################
 extensions_question = [inquirer.Checkbox('choice', message="What extensions do you want to fuzz?", choices=EXTENSIONS_CHOICES)]
 extension_as_list = inquirer.prompt(extensions_question)["choice"]
-if len(extension_as_list) == 0: 
+if len(extension_as_list) == 0:
     print(f"{Fore.YELLOW}[!] You have not selected any extension{Style.RESET_ALL}")
 
 extensions = ",".join(extension_as_list)
@@ -96,12 +100,20 @@ args = " ".join(sys.argv[1:])
 #########################
 # Running ffuf
 #########################
-if len(extensions) > 0: 
+if len(extensions) > 0:
     extension_cmd = f"-e {extensions}"
 else:
     extension_cmd = ""
 
-URL = args.split(" ")[0].replace("http://", "").replace("https://", "").replace("/FUZZ", "").replace("/", "_")
-cmd = f"ffuf -c -r -w {wordlist} -o scan-ffuf-{URL}.txt {extension_cmd} -t 64 -mc all -fc 404 -u {args}"
+url = args.split(" ")[0]
+if "FUZZ" not in url:
+    if not url.endswith("/"):
+        url += "/"
+    url += "FUZZ"
+
+other_args =  args.split(" ", 1)[1] if len(args.split(" ")) > 1 else "" 
+
+url_for_file = args.split(" ")[0].replace("http://", "").replace("https://", "").replace("/FUZZ", "").replace("/", "_")
+cmd = f"ffuf -c -r -w {wordlist} -o scans/scan-ffuf-{url_for_file}.txt {extension_cmd} -t 64 -mc all -fc 404 -u {url} {other_args}"
 print(f"[{Fore.YELLOW}*{Style.RESET_ALL}] Running command \"{Fore.WHITE}{Back.BLACK}{cmd}{Style.RESET_ALL}\"")
 os.system(cmd)
